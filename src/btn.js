b0ce07095340d559979ef84c2944c86fc387ae4f
maieltayeb <maieltayeb21@gmail.com>
2020-05-20 03:01:20 +0200

    initial commit

---
+++ src/btn.js
@@ -0,0 +1,22 @@
+import { Button } from 'react-bootstrap';
+import React, { Component } from 'react';   
+class  Btn extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+            <div>
+
+<Button variant="primary" size="lg" >
+    Button
+  </Button>{' '}
+  <Button href="#" variant="secondary" size="lg" >
+    Link
+  </Button>
+
+            </div>
+         );
+    }
+}
+ 
+export default Btn;
